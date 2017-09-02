5bfb28b87085297928d6e5fa045c542e77ef8c14
prateek27 <prateeknarang111@gmail.com>
2017-09-02 19:01:59 +0530

    added l4 codes

---
+++ Doubts/random_no_generation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+
+int main(){
+
+    srand(time(NULL));
+
+    cout<<rand()%100<<endl;
+    cout<<rand()%100<<endl;
+    cout<<rand()%100<<endl;
+
+
+}
+
+
